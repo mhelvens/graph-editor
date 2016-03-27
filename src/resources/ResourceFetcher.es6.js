d6ec215c6dab1491de8b91bd5169cf4490fd56ff
mhelvens <mhelvens@gmail.com>
2016-03-27 22:08:33 +0100

    removed some code duplication

--- src/resources/ResourceFetcher.es6.js
+++ src/resources/ResourceFetcher.es6.js
@@ -1,8 +1,8 @@
 export const request = require('../libs/superagent.es6.js').default;
-import {sw, withoutDuplicates} from '../util/misc.es6.js';
-import flatten                 from 'lodash/flatten';
-import uniq                    from 'lodash/uniq';
-import get                     from 'lodash/fp/get';
+import {sw, swf, withoutDuplicates} from '../util/misc.es6.js';
+import flatten                      from 'lodash/flatten';
+import uniq                         from 'lodash/uniq';
+import get                          from 'lodash/fp/get';
 
 // TODO: integrate this functionality with Resource.es6.js
 
@@ -12,6 +12,13 @@ const fetchResources         = Symbol('fetchResources');
 const fetchSpecificResources = Symbol('fetchSpecificResources');
 const models                 = Symbol('models');
 const modelLists             = Symbol('modelLists');
+
+const TYPE_TO_ENDPOINT = swf({
+	'LyphTemplate' : 'lyphTemplates',
+	'LayerTemplate': 'layerTemplates',
+	'CanonicalTree': 'canonicalTrees',
+	'ProcessType'  : 'processTypes'
+});
 
 let soleInstance = null;
 
@@ -26,22 +33,11 @@ export default class Resources {
 
 	async [fetchResources](type) {
 		if (!this[models][type] && !this[modelLists][type]) {
-
 			if (type === 'processTypes') {
 				this[modelLists][type] = require('../../process-types.json');
 			} else {
 				this[modelLists][type] = await request.get(`/${type}`).then(v => v.body);
 			}
-			// if (type === 'layerTemplates') {
-			// 	this[modelLists][type] = require('../../layers.json');
-			// } else {
-			// 	this[modelLists][type] = require('../../lyphtemplates.json');
-			// }
-
-
-			console.log(type, this[modelLists][type]); // TODO: remove
-
-
 			this[models][type] = {};
 			for (let model of this[modelLists][type]) {
 				this[models][type][model.id] = model;
@@ -51,21 +47,12 @@ export default class Resources {
 
 	async [fetchSpecificResources](type,ids) {
 		if (!this[models][type] && !this[modelLists][type]) {
-
 			// TODO: put ProcessType as a resource on the server
 			if (type === 'processTypes') {
 				this[modelLists][type] = require('../../process-types.json');
 			} else {
 				this[modelLists][type] = await request.get(`/${type}/${withoutDuplicates(ids).join(',')}`).then(v => v.body);
 			}
-			// if (type === 'layerTemplates') {
-			// 	this[modelLists][type] = require('../../layers.json');
-			// } else {
-			// 	this[modelLists][type] = require('../../lyphtemplates.json').filter((lt) => {
-			// 		return lt.layers.length > 0;
-			// 	});
-			// }
-
 			this[models][type] = {};
 			for (let model of this[modelLists][type]) {
 				this[models][type][model.id] = model;
@@ -82,7 +69,7 @@ export default class Resources {
 		await this[fetchSpecificResources]('lyphTemplates', uniq([
 			...        layerTemplates.map(get('lyphTemplate')),
 			...flatten(layerTemplates.map(get('materials'   ))),
-			...            treeLevels.map(get('template'))
+			...            treeLevels.map(get('template'    ))
 		]));
 		await this[fetchResources]('processTypes');
 	}
@@ -100,24 +87,14 @@ export default class Resources {
 	}
 
 	async updateResource(id, resource) {
-		let endpoint = sw(resource.type)({
-			'LyphTemplate':  'lyphTemplates',
-			'LayerTemplate': 'layerTemplates',
-			'CanonicalTree': 'canonicalTrees',
-			'ProcessType':   'processTypes'
-		});
+		let endpoint = TYPE_TO_ENDPOINT(resource.type);
 		let newResource = (await request.post(`/${endpoint}/${id}`).send(resource)).body[0];
 		Object.assign(this[models][endpoint][id], newResource);
 		return newResource;
 	}
 
 	async addNewResource(resource) {
-		let endpoint = sw(resource.type)({
-			'LyphTemplate':  'lyphTemplates',
-			'LayerTemplate': 'layerTemplates',
-			'CanonicalTree': 'canonicalTrees',
-			'ProcessType': 'processTypes'
-		});
+		let endpoint = TYPE_TO_ENDPOINT(resource.type);
 		let newResource = (await request.post(`/${endpoint}`).send(resource)).body[0];
 		this[models][endpoint][newResource.id] = newResource;
 		this[modelLists][endpoint] = [...this[modelLists][endpoint], newResource];
@@ -125,13 +102,7 @@ export default class Resources {
 	}
 
 	async deleteResource(resource) {
-		let endpoint = sw(resource.type)({
-			'LyphTemplate':  'lyphTemplates',
-			'LayerTemplate': 'layerTemplates',
-			'CanonicalTree': 'canonicalTrees',
-			'ProcessType': 'processTypes'
-		});
-
+		let endpoint = TYPE_TO_ENDPOINT(resource.type);
 		this[modelLists][endpoint] = this[modelLists][endpoint].filter(({id}) => id !== resource.id);
 		delete this[models][endpoint][resource.id];
 		await request.delete(`/${endpoint}/${resource.id}`);
